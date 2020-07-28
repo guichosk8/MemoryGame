f3ec501a4a19aeb065be2c3a5aa815da18fdd1f0
Luis González <guichosk8@gmail.com>
2020-07-27 22:12:05 -0600

    Fixing js logic code

--- app.js
+++ app.js
@@ -59,25 +59,23 @@ var cardsChosen = [];
 var cardsChosenId = [];
 var cardsWon = [];
 
-//creating gameboard
-function createBoard(){
-    for(let i=0; i < cardArray.length; i++){
-        var card = document.createElement('img');
-        card.setAttribute('src','images/blank.png');
-        card.setAttribute('data-id',i);
-        card.addEventListener('click',flipCard);
-        grid_div.appendChild(card);
+//Flip card
+function flipCard(){
+    var cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src',cardArray[cardId].img);
+    if(cardsChosen.length === 2){
+        setTimeout(checkForMatch,500);
     }
 }
-
-createBoard();
 
 //Checking for match
 function checkForMatch(){
     var cards = document.querySelectorAll('img');
     const optionOneId = cardsChosenId[0];
     const optionTwoId = cardsChosenId[1];
-    if(cardsChosen[0]===cardsChosenId[1]){
+    if(cardsChosen[0]===cardsChosen[1]){
         alert('You found a match!');
         cards[optionOneId].setAttribute('src','images/white.png');
         cards[optionTwoId].setAttribute('src','images/white.png');
@@ -95,15 +93,17 @@ function checkForMatch(){
     }
 }
 
-
-//Flip card
-function flipCard(){
-    var cardId = this.getAttribute('data-id');
-    cardsChosen.push(cardArray[cardId].name);
-    cardsChosenId.push(cardId);
-    this.setAttribute('src',cardArray[cardId].img);
-    if(cardsChosen.length === 2){
-        setTimeout(checkForMatch,500);
+//createBoard Function
+function createBoard(){
+    for(let i=0; i < cardArray.length; i++){
+        var card = document.createElement('img');
+        card.setAttribute('src','images/blank.png');
+        card.setAttribute('data-id',i);
+        card.addEventListener('click',flipCard);
+        grid_div.appendChild(card);
     }
 }
+
+createBoard();
+
 })
